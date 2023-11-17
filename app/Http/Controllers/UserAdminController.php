71db7d5d45c5ff14efdf9ac3636a7bc81db37927
GITNetgains <git@netgains.org>
2023-11-17 06:25:26 +0530

    ...

--- app/Http/Controllers/UserAdminController.php
+++ app/Http/Controllers/UserAdminController.php
@@ -1243,7 +1243,7 @@ public function addFriend(Request $req)
                 // $symp1->cycle_end_date = date('Y-m-d', strtotime($user_data->period_day . ' + ' . $user_data->average_cycle_days . ' days'));
             // }
             if(!$user_data) {
-                $symp1->cycle_start_date = $user_data["period_day"];
+                $symp1->cycle_start_date = $user_data["period_day"]->format('Y-m-d');
                 $symp1->cycle_end_date = date('Y-m-d', strtotime($user_data["period_day"] . ' + ' . $user_data->average_cycle_days . ' days'));
             }
             $symp1->save();
